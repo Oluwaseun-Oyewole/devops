cc440d25271fd53a783e8bf07e0f0227f57b84bd
Oluwaseun-Oyewole <findseunoyewole@gmail.com>
2026-07-26 06:17:17 +0000

    terraform

---
+++ terraform/examples/bootsrap.tf
@@ -0,0 +1,35 @@
+# bootstrap/main.tf
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-org-terraform-state-2026"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
